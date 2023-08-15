bd8d84f459fd9f71969125f94cda9328b5a40575
Mohamed <s2000588@edu.bc.fi>
2023-08-15 17:05:22 +0300

    add styles to navbar and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import styles from "./footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const year = new Date().getFullYear();
 
-export default Footer
+  return (
+    <div className={styles.container}>
+      <div>
+        © {year}{" "}
+        <Link href='https://www.linkedin.com/in/mohamed-capo/'>Mohamed</Link>
+      </div>
+      <div className={styles.social}>
+        <Image
+          src='/1.png'
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt=''
+        />
+        <Image
+          src='/2.png'
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt=''
+        />
+        <Image
+          src='/3.png'
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt=''
+        />
+        <Image
+          src='/4.png'
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt=''
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
