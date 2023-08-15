27db38083f3d1a725e6fcf24397f5aa7173648cd
Mohamed <s2000588@edu.bc.fi>
2023-08-15 18:01:32 +0300

    add styles to about & contact page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,26 @@
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/social_update.png";
+import Button from "@/components/button/Button";
 
 export default function Home() {
   return (
-    <main className={styles.container}>
-      <h1>hello</h1>
-    </main>
-  )
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </p>
+        <Button text='See Our Work' url='/portfolio' />
+        <button className={styles.button}>See Our Work</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt='Picture of the author' className={styles.img} />
+      </div>
+    </div>
+  );
 }
