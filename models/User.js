f78465936346b87a83f20bd779f7515377666000
Mohamed <s2000588@edu.bc.fi>
2023-08-16 20:34:25 +0300

    install mongoose and create a models

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("User", userSchema);
